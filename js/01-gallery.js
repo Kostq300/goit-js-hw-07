f56862f306fd41b8c0768c7fa78e8a622e7ca11d
Oleynik Konstantin <oleynik.kostia2@gmail.com>
2022-09-02 21:16:25 +0300

    Complete 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map((image) =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href=${image.original}>
+    <img 
+        class="gallery__image"
+        src=${image.preview}
+        data-source=${image.original}
+        alt=${image.description}
+        />
+        </a>
+        </div>`).join("");
+
+gallery.innerHTML = markup;
+
+gallery.addEventListener('click', imgClick);
+function imgClick(e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains('gallery')) return;
+
+    const bigImage = e.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+        <img src=${bigImage} width="800" height="600">
+    `)
+
+    instance.show()
+    
+    gallery.addEventListener('keydown', close);
+
+function close(e) {
+    if (e.key === 'Escape') {
+        instance.close()
+    }
+}
+    
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map((image) =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href=${image.original}>
+    <img 
+        class="gallery__image"
+        src=${image.preview}
+        data-source=${image.original}
+        alt=${image.description}
+        />
+        </a>
+        </div>`).join("");
+
+gallery.innerHTML = markup;
+
+gallery.innerHTML = markup;
+
+let galleryLightbox = new SimpleLightbox('.gallery a', {captionsData: "alt",  captionDelay: 250,});
